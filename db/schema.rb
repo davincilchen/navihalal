593cf3c3c05435c6cea93073528d9cb08d89f41c
davincilchen <doki2099@gmail.com>
2018-05-29 16:37:16 +0800

    rails generate model hashtag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_080935) do
+ActiveRecord::Schema.define(version: 2018_05_29_082903) do
 
   create_table "collects", force: :cascade do |t|
     t.integer "restaurant_id"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2018_05_29_080935) do
     t.integer "user_id"
     t.integer "friend_id"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hashtags", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "user_id"
+    t.integer "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
