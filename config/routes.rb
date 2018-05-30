f3c65f943c17cab04dd0a23283da14423420717f
victorloo <ila9970@gmail.com>
2018-05-30 22:29:23 +0800

    Setup routes for collect / uncollect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'restaurants#index'
-  resources :restaurants
+  resources :restaurants do
+    member do
+      post :collect
+      post :uncollect
+    end
+  end
   get :search, controller: :restaurants
   resources :users do
     member do
