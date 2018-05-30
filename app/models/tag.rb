20bbe5aeb8b89cf3db14b8ca3e3a95711f59dd81
davincilchen <doki2099@gmail.com>
2018-05-30 20:38:55 +0800

    remove easy function in tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,12 +2,4 @@ class Tag < ApplicationRecord
   has_many :hashtags, dependent: :destroy
   has_many :users, through: :hashtags
   has_many :restaurants, through: :hashtags
-
-  def uniq_users
-    return self.users.uniq
-  end
-
-  def uniq_restaurants
-    return self.restaurants.uniq
-  end
 end
