948f745cb94c39a4f0d2e29bac0ee73a62ee71cb
davincilchen <doki2099@gmail.com>
2018-05-31 21:27:11 +0800

    use ajax for following

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,10 +1,12 @@
 class FollowshipsController < ApplicationController
-def create
+
+  def create
+    @user = User.find(params[:following_id])
     @followship = current_user.followships.build(following_id: params[:following_id])
 
     if @followship.save
       #flash[:notice] = "Successfully followed"
-      redirect_back(fallback_location: root_path)
+      #redirect_back(fallback_location: root_path)
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
       redirect_back(fallback_location: root_path)
@@ -12,9 +14,10 @@ def create
   end
 
    def destroy
+    @user = User.find(params[:id])
     @followship = current_user.followships.where(following_id: params[:id]).first
     @followship.destroy
     #flash[:alert] = "Followship destroyed"
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 end
