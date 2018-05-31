2cf415d7f8e0b779f60d7bdfe4bb2ac5a71dab18
Tony Lin <tonylin1208@gmail.com>
2018-05-31 22:34:28 +0800

    set restaurant record in public_activity

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
+  include PublicActivity::Model
+  tracked
+  
   mount_uploader :photo, PhotoUploader
   geocoded_by :address        #從address欄位取出地址
   after_validation :geocode   #將取出的地址自動轉為經緯度分別存在 latitude、longitude 欄位
