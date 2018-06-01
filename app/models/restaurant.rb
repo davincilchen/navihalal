834dedd14a1751a794ddd1bd67bb702f7ab6a609
Tony Lin <tonylin1208@gmail.com>
2018-06-02 01:42:30 +0800

    add uniqueness of name in restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+  validates_uniqueness_of :name, :scope => :address
   geocoded_by :address        #從address欄位取出地址
   after_validation :geocode   #將取出的地址自動轉為經緯度分別存在 latitude、longitude 欄位
   after_validation :default_photo
