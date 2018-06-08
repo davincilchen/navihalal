0fbb847dbe81505bfc9aada7aa7b908965e517a3
davincilchen <doki2099@gmail.com>
2018-06-08 16:33:50 +0800

    1.global activity event 2.right sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
+  before_action :get_activities
 
+    
   # enter admin should has role = admin
   def authenticate_admin
     unless current_user.admin?
       flash[:alert] = "Not allow!"
       redirect_to root_path
+    end
+  end
+
+  private
+
+  def get_activities
+    if current_user
+      @activities = PublicActivity::Activity.all.order("created_at desc").where(owner_id: current_user.following_ids, owner_type: "User")
     end
   end
 end
