2268e4e62f60b4c274b5387f823d0916857c388e
victorloo <ila9970@gmail.com>
2018-06-14 22:08:14 +0800

    Built prototype about index and create of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @comment = Comment.new
+    @comments = @restaurant.comments
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @comment = @restaurant.comments.build(comment_params)
+    @comment.user = current_user
+    @comment.save!
+    redirect_to restaurant_comments_path(@restaurant)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+  
 end
