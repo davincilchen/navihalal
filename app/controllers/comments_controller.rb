12184c8ce09988454a3b64312b720ecb2cc17cc0
victorloo <ila9970@gmail.com>
2018-06-15 15:52:08 +0800

    User can edit and update their comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,40 @@
 class CommentsController < ApplicationController
+  before_action :set_restaurant
   
   def index
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @comment = Comment.new
+    if params[:id]
+      @comment = Comment.find(params[:id])
+    else
+      @comment = Comment.new
+    end
     @comments = @restaurant.comments
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @comment = @restaurant.comments.build(comment_params)
     @comment.user = current_user
     @comment.save!
     redirect_to restaurant_comments_path(@restaurant)
   end
 
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to restaurant_comments_path, notice: "Comment was successfully updated!"
+    else
+      @comments = @restaurant.comments
+      render 'index'
+    end
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
   
 end
